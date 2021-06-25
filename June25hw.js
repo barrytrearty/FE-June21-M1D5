3daa566a0511f89c98c5eb10fef472edf49dea83
barrytrearty <btrearty@gmail.com>
2021-06-25 15:19:33 +0100

    Started Exercise 13 (test commit)

--- June25hw.js
+++ June25hw.js
@@ -225,6 +225,8 @@ In your eCommerce you have an array of objects called shoppingCart. In this arra
 Create a function "addToShoppingCart" which receives a new object, adds it to shoppingCart and returns the total number of items in the shoppingCart.
 */
 
+const addToShoppingCart = function () {};
+
 /* EXERCISE 14
 In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
 Create a function "maxShoppingCart" which receives the shoppingCart array and returns the most expensive item in the array.
