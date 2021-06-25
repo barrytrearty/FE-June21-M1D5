3d2fff4b50c3048abdccbfe23209527d048f0ad5
barrytrearty <btrearty@gmail.com>
2021-06-25 15:16:28 +0100

    up to exercise 12 completed

--- June25hw.js
+++ June25hw.js
@@ -168,6 +168,98 @@ const giveMeRandom = function (n) {
 // console.log(giveMeRandom(7));
 // console.log(giveMeRandom(20));
 
-/* WHEN YOU ARE FINISHED
-Commit and push the code to your personal GitHub repository and share the link to your commit in Eduflow.
+/* EXERCISE 11
+Write a function "checkArray" which receives an array of random numbers (created with giveMeRandom) and prints, for each item, whether it's bigger than 5.
+The function returns the sum of the numbers bigger than 5.
+*/
+
+const checkArray = function (n) {
+  let array = giveMeRandom(n);
+  let finalSum = 0;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > 5) {
+      console.log(`${array[i]} is bigger than 5`);
+      finalSum += array[i];
+    }
+    if (array[i] == 5) {
+      console.log(`${array[i]} is equal to 5`);
+    }
+    if (array[i] < 5) {
+      console.log(`${array[i]} is small than 5`);
+    }
+  }
+  return finalSum;
+};
+
+// let checkArrayAnswer = checkArray(5);
+// console.log(checkArrayAnswer);
+
+/* EXERCISE 12
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "shippingCartTotal" which calculates the total due to the shop.
+*/
+
+let shoppingCart = [
+  {
+    price: 100,
+    name: "table",
+    id: "1111",
+    quantity: 2,
+  },
+  { price: 200, name: "tv", id: "2222", quantity: 1 },
+  { price: 50, name: "chair", id: "3333", quantity: 4 },
+];
+
+const shippingCartTotal = function (array) {
+  let totalSum = 0;
+  for (item of array) {
+    totalSum += item.price * item.quantity;
+  }
+  return totalSum;
+};
+
+// console.log(shippingCartTotal(shoppingCart));
+
+/* EXERCISE 13
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "addToShoppingCart" which receives a new object, adds it to shoppingCart and returns the total number of items in the shoppingCart.
+*/
+
+/* EXERCISE 14
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "maxShoppingCart" which receives the shoppingCart array and returns the most expensive item in the array.
+*/
+
+/* EXERCISE 15
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "latestShoppingCart" which receives the shoppingCart array and returns the last item.
+*/
+
+/* EXERCISE 16
+Create a function "loopUntil" which receives an integer x between 0 and 9.
+The function loops and prints a random number between 0 and 9 until the random number is bigger than x for three times in a row.
+*/
+
+/* EXERCISE 17
+Write a function "average" which receives an array and return the average value. The function automatically skips non-numeric entries in the array.
+*/
+
+/* EXERCISE 18
+Write a function "longest" to find the longest string from an given array of strings.
+*/
+
+/* EXERCISE 19
+Write a function to create a very simple anti spam filter for your mailbox. The function takes a string emailContent, and returns a boolean.
+Check if the email is valid using string methods. The email (in this example) is valid if the words SPAM and SCAM does not appear.
+*/
+
+/* EXERCISE 20
+Write a function that receives a date d as parameter and calculates the number of days passes since the d.
+*/
+
+/* EXERCISE 21
+Write a function "matrixGenerator" that receives x and y as parameter. The result should be a matrix of x times y with, as value, the index of the position.
+Ex.: X = 3, Y = 2
+["00","01","02"
+"10","11","12"]
 */
